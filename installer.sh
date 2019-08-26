7f7d4420b00070b46d26ba63810a0d58680637a6
Aditya Narayan <an160264@gmail.com>
2019-08-26 14:38:30 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-sudo su
 apt -y update
+echo "updated Done"
 apt -y install default-jdk
+echo "jdk install  Done"
 useradd -r -m -U -d /opt/tomcat -s /bin/false tomcat
-wget http://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.14/bin/apache-tomcat-9.0.14.tar.gz -P /tmp
+echo "user creation Done"
+wget http://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.24/bin/apache-tomcat-9.0.24.tar.gz -P /tmp
+echo "tomcat download Done"
 tar xf /tmp/apache-tomcat-9*.tar.gz -C /opt/tomcat
+echo "unzip Done"
 sudo ln -s /opt/tomcat/apache-tomcat-9.0.14 /opt/tomcat/latest
+echo "link established"
 sudo chown -RH tomcat: /opt/tomcat/latest
+echo "owner change Done"
 sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
-touch /etc/systemd/system/tomcat.service
+echo "file made executable Done"
 cat data.txt > /etc/systemd/system/tomcat.service
 systemctl daemon-reload
 systemctl start tomcat
